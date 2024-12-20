8f513cd3c13c39133f3e6707241d9c5075881543
Sonupalsingh <73596998+Sonupalsingh@users.noreply.github.com>
2024-12-20 14:28:39 +0530

    Update mariadb_user_creaetion.sh

--- mariadb_user_creaetion.sh
+++ mariadb_user_creaetion.sh
@@ -11,20 +11,21 @@ secure_mariadb() {
 ROOT_PASS=1234
 DB_USER=sonu
 DB_USER=sonu
+DB_NAME=TrainingEnquiry
 # Function to create a new MariaDB user
 create_mariadb_user() {
     # Prompt for the root password
-    read -sp "Enter the MariaDB root password: " ROOT_PASS
-    echo
+    #read -sp "Enter the MariaDB root password: " ROOT_PASS
+    #echo
 
     # Prompt for the new username and password
-    read -p "Enter the new MariaDB username: " DB_USER
-    read -sp "Enter the password for the new user: " DB_PASS
-    echo
+    #read -p "Enter the new MariaDB username: " DB_USER
+    #read -sp "Enter the password for the new user: " DB_PASS
+    #echo
 
     # Connect to MariaDB and create the user with permissions
     sudo mysql -u root -p"$ROOT_PASS" -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';"
-    sudo mysql -u root -p"$ROOT_PASS" -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'localhost' WITH GRANT OPTION;"
+    sudo mysql -u root -p"$ROOT_PASS" -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost' WITH GRANT OPTION;"
     sudo mysql -u root -p"$ROOT_PASS" -e "FLUSH PRIVILEGES;"
 
     # Print completion message
